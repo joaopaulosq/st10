757662629922c485358648809c3dd039a29e3bd3
joaopaulosq <169081465+joaopaulosq@users.noreply.github.com>
2025-09-02 15:39:48 -0300

    script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function(){
  
      let tamanhoAtualFonte = 1;
  
-     aumentaFonteBotao.addEventListener('click', function(){
+     aumentaFonteBotao.addEventListener('click', function(){  
          tamanhoAtualFonte += 0.1;
          document.body.style.fontSize = `${tamanhoAtualFonte}rem`
  
